021773a8136864dd4fc5ecaf6bf332415e97e19c
Brianda Esquivel <briandae94@gmail.com>
2023-03-01 14:39:31 -0700

    Array-practice-1
    If applied, this commit will have added replaced previous code with new code showcasing the use of arrays

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,7 @@
-alert('Hello World');
-let favoriteFood = 'Dark Chocolate';
-document.write(favoriteFood);
+// array of pokemon 
+let pokemonList = [
+{ name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+{ name: 'Eevee', height: 3, types: ['normal'] }, 
+{ name: 'Vulpix', height: 6, types: ['fire'] }, 
+{ name: 'Weedle', height: 3, types: ['bug', 'poison']}, 
+];
