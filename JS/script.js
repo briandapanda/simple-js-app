bcc859f51a1dfacd81536d5f97155d547d679551
Brianda Esquivel <briandae94@gmail.com>
2023-03-03 12:52:34 -0700

    Update JS script
    If applied, this commit will have added Pokemon list with conditional and a 'for' loop

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,17 @@
 // array of pokemon 
 let pokemonList = [
-{ name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-{ name: 'Eevee', height: 3, types: ['normal'] }, 
-{ name: 'Vulpix', height: 6, types: ['fire'] }, 
-{ name: 'Weedle', height: 3, types: ['bug', 'poison']}, 
+{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+{ name: 'Eevee', height: 0.3, types: ['normal'] }, 
+{ name: 'Vulpix', height: 0.6, types: ['fire'] }, 
+{ name: 'Weedle', height: 0.3, types: ['bug', 'poison']}, 
+{ name: 'Nidoqueen', height: 1.3, types: ['ground', 'poison']},
 ];
+// loop practice 
+for (let pokemon of pokemonList) {
+    if (pokemon.height > 1) {
+        document.write(pokemon.name + " - (Height: " + pokemon.height + ") - Yowza, that's big! <br>" ); 
+    } else {
+        document.write(pokemon.name + " - (Height: " + pokemon.height + ")<br>");
+    }
+    
+}
