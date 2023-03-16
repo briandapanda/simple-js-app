71eb20e06fb1c4b1edf9fe7cb150bc5da37a24c6
Brianda Esquivel <briandae94@gmail.com>
2023-03-16 11:56:00 -0600

    IIFE update
    If applied, this commit will have updated to include IIFE to JS

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,33 @@
 // array of pokemon 
-let pokemonList = [
-{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-{ name: 'Eevee', height: 0.3, types: ['normal'] }, 
-{ name: 'Vulpix', height: 0.6, types: ['fire'] }, 
-{ name: 'Weedle', height: 0.3, types: ['bug', 'poison']}, 
-{ name: 'Nidoqueen', height: 1.3, types: ['ground', 'poison']},
+let pokemonRepository = (function(){
+  let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    { name: 'Eevee', height: 0.3, types: ['normal'] }, 
+    { name: 'Vulpix', height: 0.6, types: ['fire'] }, 
+    { name: 'Weedle', height: 0.3, types: ['bug', 'poison']}, 
+    { name: 'Nidoqueen', height: 1.3, types: ['ground', 'poison']},
 ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'types'in pokemon) {
+        pokemonList.push(pokemon);
+    } else {
+        console.error('Invalid Pokemon Object')
+    }
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
+
 // loop practice 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 1) {
         document.write(pokemon.name + " - (Height: " + pokemon.height + ") - Yowza, that's big! <br>" ); 
     } else {
