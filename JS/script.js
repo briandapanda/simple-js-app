564bf9147c6d1d7c9d3fa9bdc9c04b6b734cad5f
Brianda Esquivel <briandae94@gmail.com>
2023-04-05 11:24:19 -0600

    PopUp Modal Update
    if applied, this commit will have fixed previous errors with the popup modal and bootstrap implementation.
    
    All pokemon should now open a pop up modal with ability to close out.

--- JS/script.js
+++ JS/script.js
@@ -24,7 +24,7 @@ let pokemonRepository = (function(){
   function addListItem(pokemon) {
     const $pokemonList = $('.pokemon-list');
     const $listItem = $('<li class="list-group-item"></li>');
-    const $button = $('<button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"></button>');
+    const $button = $('<button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" id="showDetailsButton"></button>');
   
     $button.text(pokemon.name);
     $button.click(function () {
@@ -34,79 +34,104 @@ let pokemonRepository = (function(){
     $listItem.append($button);
     $pokemonList.append($listItem);
   }
-  
   function showDetails(pokemon) {
-    console.log('showDetails function is being called!');
     loadDetails(pokemon).then(function () {
-      const modalTitle = $('.modal-title');
-      const modalBody = $('.modal-body');
+      const modalTitle = document.querySelector('.modal-title');
+      const modalBody = document.querySelector('.modal-body');
   
-      modalTitle.empty();
-      modalBody.empty();
+      modalTitle.innerHTML = '<h1>' + pokemon.name + '</h1>';
+      modalBody.innerHTML = '<img class="modal-img" src="' + pokemon.imageUrl + '">' +
+        '<p>Height: ' + pokemon.height + '</p>' +
+        '<p>Type: ' + (pokemon.types ? pokemon.types.join(', ') : '') + '</p>';
   
-      const nameElement = $('<h1>' + pokemon.name + '</h1>');
-      const imageElement = $('<img class="modal-img">');
-      imageElement.attr('src', pokemon.imageUrl);
-      const heightElement = $('<p>' + 'Height: ' + pokemon.height + '</p>');
-      const typesElement = $('<p>' + 'Types: ' + (pokemon.types ? pokemon.types.join(', ') : '') + '</p>');
-  
-      modalTitle.append(nameElement);
-      modalBody.append(imageElement);
-      modalBody.append(heightElement);
-      modalBody.append(typesElement);
-  
-      console.log('Before toggling modal:');
-      console.log($('#pokemonDetailModal'));
-  
-      $('pokemonDetailModal').modal('toggle');
-  
-      console.log('After toggling modal:');
-      console.log($('#pokemonDetailModal'));
+      $('#pokemonDetailModal').modal('toggle');
     });
   }
   
+  //gets list from API and adds pokemon as objects
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(json){
+        json.results.forEach(function(item){
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url 
+          };
+          add(pokemon);
+          addListItem(pokemon);
+        });
+        return pokemonList; 
+      })
+      .catch(function(e){
+        throw new Error('Failed to load pokemon list: ' + e.message);
+      });
+  }
   
-  
-  
-//gets list from API and adds pokemon as objects
-function loadList() {
-  return fetch(apiUrl)
-    .then(function(response){
+
+
+function loadDetails(pokemon) {
+  let url = pokemon.detailsUrl;
+  return fetch(url)
+    .then(function (response) {
       return response.json();
     })
-    .then(function(json){
-      json.results.forEach(function(item){
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url 
-        };
-        add(pokemon);
-        addListItem(pokemon);
+    .then(function (json) {
+      pokemon.imageUrl = json.sprites.front_default;
+      pokemon.height = json.height;
+      pokemon.weight = json.weight;
+      pokemon.types = json.types.map(function (type) {
+        return type.type.name;
       });
-      // returns list of pokemon after data is loaded
-      return pokemonList; 
     })
-    .catch(function(e){
+    .catch(function (e) {
       console.error(e);
     });
 }
 
+  
+  function showModal(pokemon) {
+    let modalBody = $('.modal-body');
+    let modalTitle = $('.modal-title');
+    let modalHeader = $('.modal-header');
 
-function loadDetails(pokemon){
-  let url = pokemon.detailsUrl;
-  return fetch(url)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(json){
-      pokemon.imageUrl = json.sprites.front_default;
-      pokemon.height = json.height;
-      pokemon.weight = json.weight;
-      pokemon.type = json.types[0].type.name;
-    })
-    .catch(function(e){
-      console.error(e);
+    modalTitle.empty();
+    modalBody.empty();
+  // creating element for name in modal content
+    let nameElement = $('<h1>' + pokemon.name + '</h1>');
+    //creating img
+    let imageElementFront = $('<img class="modal-img" style="width:50%">');
+    imageElementFront.attr('src', pokemon.imageUrlFront);
+    let imageElementBack = $('<img class="modal-img" style="width:50%">');
+    imageElementBack.attr('src', pokemon.imageUrlBack);
+    let heightElement = $('<p>' + 'height : ' + pokemon.height + '</p>');
+    let weightElement = $('<p>' + 'weight : ' + pokemon.weight + '</p>');
+    let typesElement = $('<p>' + 'types : ' + pokemon.types +'</p>');
+    let abilitiesElement = $('<p>' + 'abilities : ' + pokemon.abilities + '</p>');
+
+    modalTitle.append(nameElement);
+    modalBody.append(imageElementFront);
+    modalBody.append(imageElementBack);
+    modalBody.append(heightElement);
+    modalBody.append(weightElement);
+    modalBody.append(typesElement);
+    modalBody.append(abilitiesElement);
+
+    // Find the close button element
+    const $closeButton = $('<button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>');
+    const $span = $('<span aria-hidden="true">&times;</span>');
+    $closeButton.append($span);
+
+    // Add a click event listener to the close button
+    $closeButton.on('click', function () {
+      $('#exampleModal').modal('hide');
     });
+
+    // Append the close button to the modal
+    $('.modal-content').append($closeButton);
+
   }
 
   return {
@@ -119,6 +144,7 @@ function loadDetails(pokemon){
   };
 })();
 
+
 pokemonRepository.loadList().then(function(){
   // calls all Pokemon after data is loaded
   pokemonRepository.getAll().then(function(pokemonList) {
@@ -128,3 +154,8 @@ pokemonRepository.loadList().then(function(){
     });
   });
 })
+
+
+
+
+  
